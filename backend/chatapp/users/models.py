45606450f6b2c7d46d2d0c423d38d56adfbcdaba
jebbelaar <info@jorisebbelaar.nl>
2022-06-03 13:04:04 +0200

    Project setup

--- backend/chatapp/users/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class User(models.Model):
-    username = models.CharField()
-    email = models.EmailField()
-    password = models.CharField()
--- backend/chatapp/users/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-
-class UserSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    email = serializers.EmailField()
-    password = serializers.CharField()
